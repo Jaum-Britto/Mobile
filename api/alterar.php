7e502a7745e9621988a787e1db9d11f9f032c1d7
João Gabriel Pfaff de Britto <82790919+Jaum-Britto@users.noreply.github.com>
2024-09-22 16:52:14 -0300

    ahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh

--- api/alterar.php
+++ api/alterar.php
@@ -1,11 +1,11 @@
 <?php
                 $nomenovo=$_POST['nomenovo'];
                 $nomevelho=$_POST['nomevelho'];
-                $email=$_POST['email'];
-                $endereco=$_POST['endereco'];
-                $conexao =    mysqli_connect('104.198.142.105','app','usbw');
-                mysqli_select_db($conexao,'av1');
-            $sql    =    "update    cliente    set    Nome='$nomenovo',    Email='$email',    Endereco='$endereco'    where    Nome='$nomevelho'";
+                $plataforma=$_POST['plataforma'];
+                $genero=$_POST['genero'];
+                $conexao =    mysqli_connect('35.238.202.154','Mob','usbw');
+                mysqli_select_db($conexao,'jogos');
+            $sql    =    "update    jogos    set    Nome='$nomenovo',    Plataforma='$plataforma',    Genero='$genero'    where    Nome='$nomevelho'";
             $resultado    =    mysqli_query($conexao, $sql)    or die    ("Erro    .:"    .    mysqli_error($conexao));
             if($resultado)
                                         echo "1";
--- api/gravar.php
+++ api/gravar.php
@@ -1,10 +1,10 @@
 <?php
         $nome = $_POST['nome'];
-        $email = $_POST['email'];
-        $endereco =$_POST['endereco'];
-        $conexao =    mysqli_connect('104.198.142.105','app','usbw');
-        mysqli_select_db($conexao,'av1');
-        $sql    =    "insert    into    cliente    (Nome,    Email,    Endereco)    values    ('$nome','$email','$endereco')";
+        $plataforma = $_POST['plataforma'];
+        $genero =$_POST['genero'];
+        $conexao =    mysqli_connect('35.238.202.154','Mob','usbw');
+        mysqli_select_db($conexao,'jogos');
+        $sql    =    "insert    into    jogos    (Nome,    Plataforma,    Genero)    values    ('$nome','$plataforma','$genero')";
     $resposta    =    mysqli_query($conexao, $sql)    or die    ("Erro:    "    .    mysqli_error($conexao
 ));
     if($resposta)
