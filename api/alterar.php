a22f2a20dd1bf9a9f27717e466ec4e9f7a62b4d0
João Gabriel Pfaff de Britto <82790919+Jaum-Britto@users.noreply.github.com>
2024-10-05 17:47:52 -0300

    Update alterar.php

--- api/alterar.php
+++ api/alterar.php
@@ -3,7 +3,7 @@
                 $nomevelho=$_POST['nomevelho'];
                 $plataforma=$_POST['plataforma'];
                 $genero=$_POST['genero'];
-                $conexao =    mysqli_connect('35.238.202.154','Mob','usbw');
+                $conexao =    mysqli_connect('34.42.216.142','Mob','usbw');
                 mysqli_select_db($conexao,'jogos');
             $sql    =    "update    jogos    set    Nome='$nomenovo',    Plataforma='$plataforma',    Genero='$genero'    where    Nome='$nomevelho'";
             $resultado    =    mysqli_query($conexao, $sql)    or die    ("Erro    .:"    .    mysqli_error($conexao));
